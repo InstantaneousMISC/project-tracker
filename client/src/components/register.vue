da6a7cfbf563678ef9f0b591f195b93129909347
James II <jcabell2288@gmail.comm>
2019-05-25 09:09:04 -0500

    add frontend register page

---
+++ client/src/components/register.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email"
+    v-model="email"
+    placeholder="Email"
+    />
+    <input type="password" name="password"
+    v-model="password"
+    placeholder="password"
+    />
+    <button @click='register'>Register</button>
+  </div>
+</template>
+
+<script>
+import AuthService from '@/services/AuthService'
+export default {
+  data () {
+    return {
+        "email" : "",
+        "password" : ""
+    }
+  },
+  methods: {
+      async register () {
+          await AuthService.register({
+              email: this.email,
+              password: this.password
+          })
+      }
+  }, 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
